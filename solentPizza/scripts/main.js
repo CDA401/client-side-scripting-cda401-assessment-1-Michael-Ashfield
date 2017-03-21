acce609aef5facd466ff9502e5f114082bd6970b
Michael Ashfield <2ashfm61@solent.ac.uk>
2017-03-21 17:05:37 +0000

    Got time and date working, page now loading

--- solentPizza/scripts/main.js
+++ solentPizza/scripts/main.js
@@ -20,13 +20,18 @@ function convertTime(curr_hour) {
 function startTime() {
     "use strict";
     var today = new Date(),
+        curr_year = today.getFullYear(),
+        curr_month = today.getMonth() + 1, //Month starts at 0, add 1 to be accurate
+        curr_day = today.getDate(),
         curr_hour = today.getHours(),
         curr_minute = today.getMinutes(),
         curr_second = today.getSeconds();
+    curr_month = checkTime(curr_month);
+    curr_day = checkTime(curr_day);
     curr_second = checkTime(curr_second);
     curr_minute = checkTime(curr_minute);
     curr_hour = checkTime(curr_hour);
-    var curr_time = convertTime(curr_hour) + ":" + curr_minute + ":" + curr_second;
+    var curr_time = curr_day + "/" + curr_month + "/" + curr_year + " | " + convertTime(curr_hour) + ":" + curr_minute + ":" + curr_second;
     document.getElementById("time").innerHTML = curr_time;  
 }
 
