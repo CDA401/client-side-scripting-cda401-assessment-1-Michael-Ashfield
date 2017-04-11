60401a55cbc1d2b6054e1216f5fec9a75fe8f6b1
Michael Ashfield <2ashfm61@solent.ac.uk>
2017-04-11 16:13:44 +0000

    Updated alltototal function

--- solentPizza/scripts/orderscript.js
+++ solentPizza/scripts/orderscript.js
@@ -1,17 +1,24 @@
-
+var form;
+var total;
 
 function main(){
-    var form = document.getElementById("order_form");
+    form = document.getElementById("order_form");
     var sizeform = form.size_area;
-    sizeform.addEventListener("change", loopform);
-
+    sizeform.addEventListener("change", loopformsize);
 }
-function loopform(event){ //Loops size form
-    var form = document.getElementById("order_form");
+function loopformsize(event){ //Loops size form
+    form = document.getElementById("order_form");
 
     for (var i = 0; i < form.size.length; i++){
         if (form.size[i].checked){
             console.log("You selected " + form.size[i].value);
+            addToOrder(form.size[i]);
         }
     }
+}
+
+function addToOrder(item){ //Adds item to total and list
+    total += parseInt(item.dataset.price);
+    console.log("total= " + total);
+    
 }
