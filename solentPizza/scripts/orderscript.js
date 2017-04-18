c95f73e073bfad6a0ad1db8b6fda0fba3a396fb7
Michael Ashfield <2ashfm61@solent.ac.uk>
2017-04-18 22:11:16 +0000

    Added formloop, totals, and toppings

--- solentPizza/scripts/orderscript.js
+++ solentPizza/scripts/orderscript.js
@@ -1,24 +1,45 @@
 var form;
-var total;
+var total = 0;
 
 function main(){
     form = document.getElementById("order_form");
     var sizeform = form.size_area;
-    sizeform.addEventListener("change", loopformsize);
+    var toppingform = form.topping_area;
+    form.addEventListener("change", loopForm);
 }
-function loopformsize(event){ //Loops size form
-    form = document.getElementById("order_form");
 
+function loopForm() { //Loops whole form when any part updated
+    total = 0 //Resets total so it can be recalculated
+    loopFormSize();
+    loopFormTopping();
+    console.log("total price= " + total);
+}
+
+function loopFormSize(event){ //Loops size form
     for (var i = 0; i < form.size.length; i++){
         if (form.size[i].checked){
             console.log("You selected " + form.size[i].value);
-            addToOrder(form.size[i]);
+            addToOrder(form.size[i]); //Adds to order summery
+            addToTotal(form.size[i].dataset.price) //Adds to total price
         }
     }
 }
 
-function addToOrder(item){ //Adds item to total and list
-    total += parseInt(item.dataset.price);
-    console.log("total= " + total);
-    
+function loopFormTopping(event){ //Loops size form
+    for (var i = 0; i < form.topping.length; i++){
+        if (form.topping[i].checked){
+            console.log("You selected " + form.topping[i].value);
+            addToOrder(form.topping[i]); //Adds to order summery
+            addToTotal(form.topping[i].dataset.price) //Adds to total price
+        }
+    }
+}
+
+function addToTotal(cost){
+    total += parseInt(cost);
+    return total;
+}
+
+function addToOrder(item){ //Adds item to total and list FINISH ORDER SUMMERY
+    console.log("item cost= " + item.dataset.price);
 }
