5c88647440fa1329a7776a2c5744428bc8724ef0
Michael Ashfield <2ashfm61@solent.ac.uk>
2017-05-13 18:16:24 +0000

    Fixed carousel, all functionality working for home page

--- solentPizza/scripts/main.js
+++ solentPizza/scripts/main.js
@@ -1,5 +1,5 @@
-var myTimer = setInterval(startTime, 500);
-var slideTimer = setInterval(slideImage, 3000);
+var myTimer = setInterval(startTime, 500); //Half second update for clock
+var slideTimer = setInterval(slideImage, 5000); //5 second loop for carousel
 
 function main(){
     document.getElementById("discountImg").addEventListener("mouseover", displayDiscountOn);
@@ -13,14 +13,14 @@ function checkTime(i) { // add zero in front of numbers < 10
     return i;
 }
 
-function convertTime(curr_hour) {
+function convertTime(curr_hour) { //Makes clock 12 hour
     if (curr_hour >= 12) {
         return (curr_hour - 12);
     }
 }
 
 function startTime() {
-    var today = new Date(),
+    var today = new Date(), //Gets the fresh time and date and assigns them to variables
         curr_year = today.getFullYear(),
         curr_month = today.getMonth() + 1, //Month starts at 0, add 1 to be accurate
         curr_day = today.getDate(),
@@ -32,8 +32,8 @@ function startTime() {
     curr_second = checkTime(curr_second);
     curr_minute = checkTime(curr_minute);
     curr_hour = checkTime(curr_hour);
-    var curr_time = curr_day + "/" + curr_month + "/" + curr_year + " | " + convertTime(curr_hour) + ":" + curr_minute + ":" + curr_second;
-    document.getElementById("time").innerHTML = curr_time;  
+    var curr_time = curr_day + "/" + curr_month + "/" + curr_year + " | " + convertTime(curr_hour) + ":" + curr_minute + ":" + curr_second; //Puts time into 1 variable
+    document.getElementById("time").innerHTML = curr_time;  //Puts time into html
 }
 
 function displayDiscountOn(){//Rollover image
@@ -44,13 +44,12 @@ function displayDiscountOff(){//Rolloff image
     document.getElementById("discountImg").src = "images/dailyDiscount.png";
 }
 
-function slideImage(){
-    var myList = ["slider1.png","slider2.png","slider3.png","slider4.png","slider5.png"];
-    var currItem = 0;
-    document.getElementById("carousel").src = myList[currItem];
+    var currItem = 0;//Outside of function to prevent it being 0 every time function is called
+function slideImage(){ //Image carousel, loops between 5 images
+    var myList = ["slider1.png","slider2.png","slider3.png","slider4.png","slider5.png"]; //Array with all images
+    document.getElementById("carousel").src = "images/" + myList[currItem]; //Change image
     currItem += 1;
-    if (currItem == 5){
+    if (currItem == 5){ //reset currItem to 0 when at end of array
         currItem = 0;
     }
-    console.log(myList[currItem])
 }
